c7c1064075669be4539d5fbc435bfb9ff259365b
Chris Atkinson <catkinson@gmail.com>
2017-08-22 22:08:33 +0100

    Rest interface fade up and down

--- Rakolighting/RestBridge.py
+++ Rakolighting/RestBridge.py
@@ -8,3 +8,15 @@ bridge = Bridge.Bridge()
 @app.route("/level/<int:room>/<int:channel>/<int:level>")
 def rako(room, channel, level):
     return bridge.level(room, channel, level)
+
+
+@app.route("/fade_down/<int:room>/<int:channel>")
+@app.route("/fade_down")
+def fade_down(room=0, channel=0):
+    return bridge.fade_down(room, channel)
+
+
+@app.route("/fade_up/<int:room>/<int:channel>")
+@app.route("/fade_up")
+def fade_up(room=0, channel=0):
+    return bridge.fade_up(room, channel)
